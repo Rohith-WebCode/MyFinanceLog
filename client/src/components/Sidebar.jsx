804ebacf894ba81c2d7ec811ef84be87e0fc1168
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-08-05 21:44:34 +0530

    setup frontend with Vite, Tailwind CSS, and layout structure

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from "../assets/Finance_Service_Logo.png"
+
+const Sidebar = () => {
+  return (
+    <div className='p-4 w-6xl'>
+        <div className='flex items-center'>
+        <img src={Logo} className='w-10'/>
+        <h1 className='text-xl font-bold'>MyFinanceLog</h1>
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Sidebar
