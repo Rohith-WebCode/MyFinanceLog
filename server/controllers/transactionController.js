71cfa5a033aa8903a6084760783dbcf02753ab3c
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-08-05 14:00:17 +0530

    add search functionality for transactions by title and category

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -46,5 +46,26 @@ const deleteTransaction  =async (req,res)=>{
   }
 }
 
+const searchTransaction = async (req,res)=>{
+  const search = req.query.search;
+  try {
+    const results = await Transaction.find({
+      $or:[
+        {title:{$regex:search, $options: 'i'}},
+        { category: { $regex: search, $options: 'i' } }
+      ]
+    })
 
-module.exports = {addTransaction,getTransactions,deleteTransaction}
+      if (results.length === 0) {
+      return res.status(404).json({ message: 'No transactions found.' });
+    }
+
+ res.status(200).json({results});
+  } catch (error) {
+      res.status(500).json({ message: 'Failed to fetch transactions', error: error.message });
+  }
+
+}
+
+
+module.exports = {addTransaction,getTransactions,deleteTransaction,searchTransaction}
--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const Router = express.Router()
-const {addTransaction,getTransactions,deleteTransaction} = require ('../controllers/transactionController')
+const {addTransaction,getTransactions,deleteTransaction,searchTransaction} = require ('../controllers/transactionController')
 const protect = require('../middleware/protect')
 
 
 Router.post('/transactions',protect,addTransaction)
 Router.get('/transactions',protect,getTransactions)
 Router.delete('/transactions/:id',protect,deleteTransaction)
+Router.get('/transactions/search',protect,searchTransaction)
 
 module.exports = Router
