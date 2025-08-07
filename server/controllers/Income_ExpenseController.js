6e1993d3b1556696260106426049f4d274f31951
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-08-07 22:21:38 +0530

    feat: Implemented API pagination for income, expense, 30-day expenses, and 60-day income routes to improve performance and scalability

--- server/controllers/Income_ExpenseController.js
+++ server/controllers/Income_ExpenseController.js
@@ -1,50 +1,107 @@
 const Transaction = require("../models/TransactionSchema")
 
 const getIncomeTransactions= async(req,res)=>{
+
+    const page = Math.max(parseInt(req.query.pages) || 1,1)
+    const limit = 10
+    const skip = (page-1)*limit
     try {
-        const income = await Transaction.find({userId:req.user,type:'income'}).sort({date:-1})
-        res.status(200).json({income})
+        const [transaction,total] = await Promise.all([
+            Transaction.find({userId:req.user,type:"income"})
+            .sort({date:-1})
+            .skip(skip)
+            .limit(limit),
+            Transaction.countDocuments({userId:req.user})
+        ])
+        res.status(200).json({
+            transaction,
+            page,
+            totalPages:Math.ceil(total/limit),
+            total
+        })
     } catch (error) {
          res.status(500).json({ message: 'Failed to fetch income transactions', error: err.message });
     }
 }
 
 const getExpenseTransactions = async (req,res) =>{
+    const page = Math.max(parseInt(req.query.pages) || 1,1)
+    const limit = 10
+    const skip = (page-1)*limit
     try {
-        const expense =  await Transaction.find({userId:req.user,type:'expense'}).sort({date:-1})
-        res.status(200).json({expense})
+        const [transaction,total] = await Promise.all([
+            Transaction.find({userId:req.user,type:"expense"})
+            .sort({date:-1})
+            .skip(skip)
+            .limit(limit),
+            Transaction.countDocuments({userId:req.user})
+        ])
+        res.status(200).json({
+            transaction,
+            page,
+            totalPages:Math.ceil(total/limit),
+            total
+        })
     } catch (error) {
          res.status(500).json({ message: 'Failed to fetch expense transactions', error: err.message });
     }
 }
 
 const getLast30DaysExpenses = async(req,res)=>{
+    const pages = Math.max(parseInt(req.query.pages) || 1,1)
+    const limit = 10
+    const skip = (pages - 1) *limit
     try {
+
         const thirtyDaysAgo  = new Date()
         thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30)
-        const expense = await Transaction.find({
-            userId:req.user,
-            type:'expense',
-            date:{$gte:thirtyDaysAgo}
-        }).sort({date:-1})
-        res.status(200).json({expense})
+
+        const [transaction,total] = await Promise.all([
+        Transaction.find({  
+                    userId:req.user,
+                    type:'expense',
+                    date:{$gte:thirtyDaysAgo}
+                })
+                .sort({date:-1})
+                .skip(skip)
+                .limit(limit),
+                Transaction.countDocuments({userId:req.user})     
+        ]) 
+        res.status(200).json({
+            transaction,
+            pages,
+            totalPages:Math.ceil(total/limit),
+            total
+        })
     } catch (error) {
          res.status(500).json({ message: 'Failed to fetch 30 days expense transactions', error: err.message });
     }
     
 }
 const getLast60DaysIncome = async(req,res)=>{
+    const pages = Math.max(parseInt(req.query.pages) || 1,1)
+    const limit = 10
+    const skip = (pages - 1) *limit
 
 try {
     const sixtyDaysAgo = new Date()
     sixtyDaysAgo.setDate(sixtyDaysAgo.getDate()- 60)
-
-    const income = await Transaction.find({
-        userId:req.user,
-        type:"income",
-        date:{$gte:sixtyDaysAgo}
-    }).sort({date:-1})
-    res.status(200).json({income})
+    const [transaction,total] = await Promise.all([
+        Transaction.find({
+            userId:req.user,
+            type:"income",
+            date:{$gte:sixtyDaysAgo}
+        }).sort({date:-1})
+        .skip(skip)
+        .limit(limit),
+        Transaction.countDocuments({userId:req.user})    
+    ])
+    res.status(200).json({
+        transaction,
+        pages,
+        totalPages:Math.ceil(total/limit),
+        total
+    })
 } catch (error) {
      res.status(500).json({ message: 'Failed to fetch 60 days expense transactions', error: err.message });
 }
