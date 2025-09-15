0dcae1044e0216fc7d0aa7fff677e50b68b71019
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-09-15 10:39:55 +0530

    make chenges in token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -42,8 +42,8 @@ const loginUser = async (req,res)=>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
